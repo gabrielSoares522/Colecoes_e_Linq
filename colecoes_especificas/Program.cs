8b5d11b9cd61d5f5073857258899569d98d49942
gabrielSoares522 <gabrielcsoares52@gmail.com>
2022-02-27 10:02:08 -0300

    addicionando exercicio Fila

---
+++ colecoes_especificas/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+Queue<string> Fila = new Queue<string>();
+
+Fila.Enqueue("Gabriel");
+Fila.Enqueue("Diego");
+Fila.Enqueue("Arthur");
+
+while(Fila.Count >0){
+    Console.WriteLine("Proximo a sair é {0}",Fila.Peek());
+    Console.WriteLine("Retirando {0}",Fila.Dequeue());
+}
+
+Console.WriteLine("Pessoas na fila {0}",Fila.Count);
