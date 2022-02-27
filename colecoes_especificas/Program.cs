32cc410a97e5e5357aafcc3ed8a55073aab522c2
gabrielSoares522 <gabrielcsoares52@gmail.com>
2022-02-27 11:14:49 -0300

    addicionando exercicio Pilha

--- colecoes_especificas/Program.cs
+++ colecoes_especificas/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 
+Console.WriteLine("EXEMPLO DE FILA");
 Queue<string> Fila = new Queue<string>();
 
 Fila.Enqueue("Gabriel");
@@ -12,3 +13,18 @@ while(Fila.Count >0){
 }
 
 Console.WriteLine("Pessoas na fila {0}",Fila.Count);
+
+
+Console.WriteLine();
+Console.WriteLine("EXEMPLO DE PILHA");
+Stack<string> Pilha = new Stack<string>();
+
+Pilha.Push("Duna");
+Pilha.Push("Blade Runner");
+Pilha.Push("1984");
+
+while(Pilha.Count >0){
+    Console.WriteLine("Proximo a sair é {0}",Pilha.Peek());
+    Console.WriteLine("Retirando {0}",Pilha.Pop());
+}
+Console.WriteLine("Pessoas na Pilha {0}",Pilha.Count);
